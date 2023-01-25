b68f744091e691c17e0581926f0ecdf5f12cd845
Poliana Elizabeth <polianaelizabeth@gmail.com>
2023-01-25 08:49:49 -0300

    primeiro commit

---
+++ ex08.py
@@ -0,0 +1,7 @@
+valor1 = int(input("digite um valor: "))
+valor2 = int(input("digite outro valor: "))
+
+if valor1 > valor2:
+    print("maior valor: ", valor1)
+else:
+    print("maior valor: ", valor2)
